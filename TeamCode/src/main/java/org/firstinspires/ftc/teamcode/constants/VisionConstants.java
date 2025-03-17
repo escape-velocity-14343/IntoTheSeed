2564d4f7ee121c4c555fe59605080534fe299ff4
William <wangwilli1bsd@gmail.com>
2025-03-17 14:18:21 -0700

    shit load of changes - brian yu

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/constants/VisionConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/constants/VisionConstants.java
@@ -3,6 +3,14 @@ package org.firstinspires.ftc.teamcode.constants;
 public class VisionConstants {
     //1280 x 800
     public static String slideCameraName = "camera";
+    public static String chassisCameraName = "chassiscamera";
     public static int width = 1280;
-    public static int height = 800;
+    public static int height = 720;
+
+    //Supported resolutions 1280 720
+    // 800 600
+    // 960 540
+    // 640 840
+
+    public static int defaultWhiteBalance = 3000;
 }
