e611659dbf4f6904b99796f270792b4722a9b492
William H Wang <61762213+cyblazer@users.noreply.github.com>
2025-03-23 21:39:46 -0700

    again

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/constants/IVKConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/constants/IVKConstants.java
@@ -2,7 +2,7 @@ package org.firstinspires.ftc.teamcode.constants;
 
 public class IVKConstants {
     public static Double pivotPointHeight = 3.4; //in inches
-
+    public static Double slideLength = 12.0;
     public static Double lengthOfBot = 13.38; // in inches
     public static Double widthOfBot = 12.94; // in inches
 }
