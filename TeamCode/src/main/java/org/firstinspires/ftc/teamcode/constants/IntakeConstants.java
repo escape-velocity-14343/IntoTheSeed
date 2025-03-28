636601f2e4466209478777a89d1b91f9a4b37344
Allen <allenftc@outlook.com>
2025-03-28 15:02:42 -0700

    michael constants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/constants/IntakeConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/constants/IntakeConstants.java
@@ -27,7 +27,7 @@ public class IntakeConstants {
     public static double closedPos = 0.9;
     public static double singleIntakePos = 0.85;
 
-    public static double clawOffset = 0.03;
+    public static double clawOffset = 0.015;
 
     // turret constants
     public static double minAngle = -130;
